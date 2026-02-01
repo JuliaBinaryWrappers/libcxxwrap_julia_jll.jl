8b3593e863ecf6bc84730cb560174f95f63151c3
jlbuild <juliabuildbot@gmail.com>
2026-02-01 21:28:22 +0000

    libcxxwrap_julia_jll build 0.14.8+0

--- src/libcxxwrap_julia_jll.jl
+++ src/libcxxwrap_julia_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libcxxwrap_julia")
-JLLWrappers.@generate_main_file("libcxxwrap_julia", UUID("3eaa8342-bff7-56a5-9981-c04077f7cee7"))
+JLLWrappers.@generate_main_file("libcxxwrap_julia", Base.UUID("3eaa8342-bff7-56a5-9981-c04077f7cee7"))
 end  # module libcxxwrap_julia_jll
