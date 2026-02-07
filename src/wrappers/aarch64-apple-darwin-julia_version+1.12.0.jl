d11e8c6bf81f536bda10ac6a32892a4f5c000502
jlbuild <juliabuildbot@gmail.com>
2026-02-07 12:31:42 +0000

    libcxxwrap_julia_jll build 0.14.9+0

--- src/wrappers/aarch64-apple-darwin-julia_version+1.12.0.jl
+++ src/wrappers/aarch64-apple-darwin-julia_version+1.12.0.jl
@@ -8,7 +8,7 @@ function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libcxxwrap_julia,
-        "lib/libcxxwrap_julia.0.14.8.dylib",
+        "lib/libcxxwrap_julia.0.14.9.dylib",
         RTLD_GLOBAL,
     )
 
